0d73d5d54a866d4a66c57259a6971ee3f44c11fb
Quvonchbek Umurzoqov <quvonchbek.dev@gmail.com>
2024-02-17 11:38:57 +0500

    post data format changed from url to json

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse, RedirectResponse
+from pydantic import BaseModel
 
 from utils import tg_api
 
 app = FastAPI()
+
+
+class Message(BaseModel):
+    phone: str
+    message: str
+    name: str | None = None
 
 
 @app.get("/")
@@ -12,8 +19,8 @@ async def root():
 
 
 @app.post("/sendMessage/")
-async def send_message(phone: str, message: str, name: str = "Telegram User"):
-    sent = await tg_api.send_message(phone, message, name)
+async def send_message(msg: Message):
+    sent = await tg_api.send_message(msg.phone, msg.message, msg.name or "Telegram User")
     if sent:
         return JSONResponse({"msg": "Successfully sent"}, status_code=200)
     return JSONResponse({"msg": "Number not found"}, status_code=406)
