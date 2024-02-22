6d6ed1967fff247daae2feef7c2c2c89dcdb86c2
Quvonchbek Umurzoqov <quvonchbek.dev@gmail.com>
2024-02-22 19:50:41 +0500

    Drf Openapi docs ready

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from api.views import TestView, SendMessageView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Telegram Userbot API",
+        default_version='v1',
+        description="Telegram userbot API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/test', TestView.as_view(), name="test_view"),
+    path('api/v1/sendMessage', SendMessageView.as_view(), name="send_message")
 ]
