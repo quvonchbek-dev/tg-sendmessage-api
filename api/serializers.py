7d6de20b749b8e1b118d1f67faf2f72520d8ce47
Quvonchbek Umurzoqov <quvonchbek.dev@gmail.com>
2024-03-04 19:16:06 +0500

    new serializers location, send code

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
 
+class LocationSerializer(serializers.Serializer):
+    latitude = serializers.FloatField()
+    longitude = serializers.FloatField()
+
+
 class MessageSerializer(serializers.Serializer):
     chatId = serializers.CharField(max_length=30, allow_blank=True, allow_null=True, default="")
     merchantId = serializers.CharField(max_length=255)
@@ -10,18 +15,24 @@ class MessageSerializer(serializers.Serializer):
     firstName = serializers.CharField(max_length=100, allow_null=True, allow_blank=True, default="Firstname")
     lastName = serializers.CharField(max_length=100, allow_null=True, allow_blank=True, default="Lastname")
     message = serializers.CharField(max_length=None, min_length=1, required=True)
+    location = LocationSerializer(allow_null=True, required=False)
     timestamp = serializers.IntegerField()
     hash = serializers.CharField(max_length=255)
 
 
-class NewAccountSerializer(serializers.Serializer):
-    merchant_name = serializers.CharField(max_length=255)
+class SendCodeSerializer(serializers.Serializer):
+    account_name = serializers.CharField(max_length=255)
     merchant_id = serializers.UUIDField()
     phone = serializers.CharField(max_length=30, default="")
 
 
-class OTPSerializer(serializers.Serializer):
-    phone = serializers.CharField(max_length=30, default="")
+class ResendCodeRequestSerializer(SendCodeSerializer):
     phone_code_hash = serializers.CharField(max_length=255, default="")
+
+
+class CheckCodeSerializer(ResendCodeRequestSerializer):
     phone_code = serializers.CharField(max_length=10, default="")
-    password = serializers.CharField(max_length=255, allow_null=True, allow_blank=True)
+
+
+class CheckPasswordSerializer(SendCodeSerializer):
+    password = serializers.CharField(max_length=255)
