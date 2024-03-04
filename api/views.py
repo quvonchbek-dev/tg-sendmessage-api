b1fa83e41d3dfba569725fa63d1e67669fc9bf73
Quvonchbek Umurzoqov <quvonchbek.dev@gmail.com>
2024-03-04 19:18:26 +0500

    New telegram views

--- api/views.py
+++ api/views.py
@@ -1,11 +1,19 @@
+import asyncio
+import json
 from collections import OrderedDict
 
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import JsonResponse
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView, Response, Request
 
 from utils.functions import check_hash
 from . import enums
-from .serializers import MessageSerializer
+from . import serializers
+from .tgsession import TgSession
+
+sessions = {}
 
 
 class TestView(APIView):
@@ -14,9 +22,9 @@ class TestView(APIView):
 
 
 class SendMessageView(APIView):
-    @swagger_auto_schema(request_body=MessageSerializer)
+    @swagger_auto_schema(request_body=serializers.MessageSerializer)
     def post(self, request: Request):
-        serializer = MessageSerializer(data=request.data)
+        serializer = serializers.MessageSerializer(data=request.data)
         if not serializer.is_valid(raise_exception=True):
             status = enums.CustomStatusCodes.VALIDATION_ERROR
             return Response(dict(status=status.dict(), msgId=None))
@@ -34,9 +42,9 @@ class SendMessageView(APIView):
 
 
 class AddTelegramAccount(APIView):
-    @swagger_auto_schema(request_body=MessageSerializer)
+    @swagger_auto_schema(request_body=serializers.MessageSerializer)
     def post(self, request: Request):
-        serializer = MessageSerializer(data=request.data)
+        serializer = serializers.MessageSerializer(data=request.data)
         if not serializer.is_valid(raise_exception=True):
             status = enums.CustomStatusCodes.VALIDATION_ERROR
         data: OrderedDict = serializer.validated_data
@@ -44,6 +52,76 @@ class AddTelegramAccount(APIView):
         if not ok:
             status = enums.CustomStatusCodes.INVALID_CREDENTIALS
             return Response(dict(status=status.dict()))
+
+
+# @swagger_auto_schema(request_body=serializers.SendCodeSerializer)
+async def send_code_view(request: WSGIRequest):
+    data = json.loads(request.body)
+    serializer = serializers.SendCodeSerializer(data=data)
+    if not serializer.is_valid():
+        return JsonResponse({"error": "ValidationError"})
+    try:
+        data = serializer.validated_data
+        phone = data.get("phone")
+        account_name = data.get("account_name")
+        if sessions.get(phone):
+            try:
+                del sessions[phone]
+            except Exception as ex:
+                return JsonResponse({"error": ex})
+        tg_session = TgSession(account_name, phone)
+        await tg_session.init()
+        code_hash = await tg_session.send_code()
+        sessions[phone] = tg_session
+        return JsonResponse({"phone_code_hash": code_hash})
+    except asyncio.CancelledError:
+        return JsonResponse({"hello": "asyncio.CancelledError"})
+    except Exception as ex:
+        return JsonResponse(dict(error=str(ex)))
+
+
+@swagger_auto_schema(request_body=serializers.CheckCodeSerializer)
+async def check_code_view(request: WSGIRequest):
+    serializer = serializers.CheckCodeSerializer()
+    try:
+        return JsonResponse({"hello": "CheckCodeSerializer"})
+    except asyncio.CancelledError:
+        raise
+
+
+@swagger_auto_schema(request_body=serializers.CheckPasswordSerializer)
+async def check_password_view(request: WSGIRequest):
+    serializer = serializers.CheckPasswordSerializer()
+    try:
+        return JsonResponse({"hello": "CheckPasswordSerializer"})
+    except asyncio.CancelledError:
+        raise
+
+
+# @api_view(http_method_names=['POST'])
+async def resend_code_view(request: WSGIRequest):
+    data = json.loads(request.body)
+    serializer = serializers.ResendCodeRequestSerializer(data=data)
+    if not serializer.is_valid():
+        return JsonResponse({"error": "ValidationError"})
+    try:
+        data = serializer.validated_data
+        phone = data.get("phone")
+        account_name = data.get("account_name")
+        if sessions.get(phone):
+            try:
+                del sessions[phone]
+            except Exception as ex:
+                return JsonResponse({"error": ex})
+        tg_session = TgSession(account_name, phone)
+        await tg_session.init()
+        code_hash = await tg_session.send_code()
+        sessions[phone] = tg_session
+        return JsonResponse({"phone_code_hash": code_hash})
+    except asyncio.CancelledError:
+        return JsonResponse({"hello": "asyncio.CancelledError"})
+    except Exception as ex:
+        return JsonResponse(dict(error=str(ex)))
 
 
 if __name__ == "__main__":
